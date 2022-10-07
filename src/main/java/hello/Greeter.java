cb650eb3e3ed4ef660ba3a87c71db487c04737f9
Palec <kubalikpavol@gmai.com>
2022-10-07 11:15:02 +0200

    maven

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,55 +1,7 @@
 package hello;
 
-
-
 public class Greeter {
-
-
-
-  private String name = "";
-
-
-
-  public String getName() 
-
-  {
-
-    return name;
-
-  }
-
-
-
-  public void setName(String name) 
-
-  {
-
-      this.name = name;
-
-  }
-
-
-
-  public String sayHello() 
-
-  {
-
-  	if (name == "") 
-
-    {
-
-       return "Hello!";
-
+    public String sayHello() {
+        return "DevOps Project for AchiStar Technologies";
     }
-
-    else 
-
-    {
-
-       return "Hello " + name + "!";
-
-    }
-
-  }
-
 }
